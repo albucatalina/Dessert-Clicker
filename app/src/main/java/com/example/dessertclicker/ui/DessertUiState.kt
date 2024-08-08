2f1e56abca738567510665d49aa89146969fc736
Albu Catalina <87013834+albucatalina@users.noreply.github.com>
2024-08-08 22:49:36 +0300

    Add a UI state class
    Create a data class that holds all the necessary data for the UI

---
+++ app/src/main/java/com/example/dessertclicker/ui/DessertUiState.kt
@@ -0,0 +1,9 @@
+package com.example.dessertclicker.ui
+
+data class DessertUiState(
+    val revenue: Int = 0,
+    val dessertSold: Int = 0,
+    val currentDessertIndex: Int = 0,
+    val currentDessertPrice: Int = 0,
+    val currentDessertImageId: Int = 0
+)
